f263d95e9a456971105127e68d7824d7b55b969e
Parker Morse <pmorse@concord.org>
2013-12-02 21:09:17 -0500

    First pass at parameterizing error div name

--- jquery.html-validation.js
+++ jquery.html-validation.js
@@ -7,8 +7,12 @@
 */
 
 (function ( $ ) {
+    var defaults = {
+        errorDiv: '#errors'
+    },
+
     // Helper functions
-    var validation = function ( text ) {
+    validation = function ( text ) {
         var xmlDoc, parser;
         if ( window.DOMParser ) {
             parser = new DOMParser();
@@ -26,11 +30,11 @@
             var errors = validation( '<div>' + $( this ).val() + '</div>' );
             if ($(errors).find('parsererror').length > 0) {
                 // TODO: Customize this
-                $('#errors').html( '<p style="padding: 3px; background-color: #ffcccc; border: 1px solid #ff0000; font-weight: bold; color: #bb3333;">There were parse errors in the HTML; please correct them before saving.</p>');
-                $('#errors').append( '<div class="error-detail" style="display: none;"></div>');
-                $('#errors .error-detail').append( errors.children );
+                $(defaults.errorDiv).html( '<p style="padding: 3px; background-color: #ffcccc; border: 1px solid #ff0000; font-weight: bold; color: #bb3333;">There were parse errors in the HTML; please correct them before saving.</p>');
+                $(defaults.errorDiv).append( '<div class="error-detail" style="display: none;"></div>');
+                $(defaults.errorDiv + ' .error-detail').append( errors.children );
             } else {
-                $('#errors').html('');
+                $(defaults.errorDiv).html('');
             }
         });
     };
